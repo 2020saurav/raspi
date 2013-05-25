6b104700738b541475ea61715c4cdd9bfacda6ba
Proneet Verma <proneetv@iitk.ac.in>
2013-05-25 14:31:48 +0530

    Update houghcircles.py

--- houghcircles.py
+++ houghcircles.py
@@ -1,16 +1,3 @@
-''' file name : houghcircles.py
-
-Description : This sample shows how to detect circles in image with Hough Transform
-
-This is Python version of this tutorial : http://opencv.itseez.com/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html
-
-Level : Beginner
-
-Benefits : Learn to find circles in the image and draw them
-
-Usage : python houghcircles.py 
-
-Written by : Abid K. (abidrahman2@gmail.com) , Visit opencvpython.blogspot.com for more tutorials '''
 
 import cv2
 import numpy as np
